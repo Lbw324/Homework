9d42da396e826b3953e17ce6c7fe350e88d08704
Lbw324 <2551088471@qq.com>
2026-09-23 21:29:43 +0800

    算法组第二次作业第七题

---
+++ hw07.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+# include <string>
+
+int main(){
+    std::string s;
+    std::cin>>s;
+    int x=0,y=0;
+    for(int i=0;i<s.length();i++){
+        if(s[i] == 'U'){
+            y++;
+        }
+        else if(s[i] == 'D'){
+            y--;
+        }
+        else if(s[i] == 'L'){
+            x--;
+        }
+        else if(s[i] == 'R'){
+            x++;
+        }
+    }
+    if(x==0&&y==0){
+        std::cout<<"true";
+    }
+    else{
+        std::cout<<"false";
+    }
+}
